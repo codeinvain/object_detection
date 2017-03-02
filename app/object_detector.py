added faker for network tables using signals writing process pid to tmp fixed ratio calculation calculating cam input by request from NetworkManager Geometry mock fixed communication

app/object_detector.py
@@ -4,7 +4,8 @@ import numpy as np
 import imutils
 import tracks
 from target import Target
-
+from geometry import Geometry
+from communication import TableManager
 
 class ObjectDetector:
 
@@ -13,19 +14,21 @@ class ObjectDetector:
 
 
     def __init__(self):
-        
         tracks.logger.debug(""" opencv version: """+cv2.__version__)
         tracks.logger.info("capturing camera port "+str(tracks.config["camera"]))
+        self._detection_status = False
         firstFrame = None
         camera = cv2.VideoCapture(tracks.config['camera'])
         low = np.array(tracks.config['detection']['from_color']) # [h,v,s] color format
         heigh = np.array(tracks.config['detection']['to_color'])
         blur_radius = tracks.config['detection']['blur_radius']
+        self.geometry = Geometry()
+        self.comm = TableManager(self.set_detection_status)
 
         while True:
             (grabbed, frame) = camera.read()
-            if not grabbed:
-                break
+            if not grabbed or self._detection_status==False:
+                continue
 
             # blure image 
             blur = cv2.blur(frame, (blur_radius,blur_radius))
@@ -46,16 +49,27 @@ class ObjectDetector:
             cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
 
             if (len(cnts)>1):
-                self.find_and_report_target(cnts)
+                self.find_and_report_target(cnts,mask)
 
 
-    def find_and_report_target(self,cnts):
+    def set_detection_status(self,status):
+        tracks.logger.debug('setting detection status to {0}'.format(status))
+        self._detection_status=status
+
+
+    def find_and_report_target(self,cnts,mask):
         targets = list(map(lambda c: Target(c) ,cnts))
-        pair = self.find_pair(targets)
+        potential = list(filter(lambda t: t.isValid(),targets))
+        pair = self.find_pair(potential)
+        if (len(pair)==2):
+            coordinates = self.geometry.get_coordinates(pair,mask)
+            self.comm.publish_target_data(coordinates.x_distance,coordinates.y_distance,coordinates.angle)
 
 
 
     def find_pair(self,targets):
+        if (len(targets)==2):
+            return list(targets)
         pair = []
         left  = list(targets)
         right = list(targets)